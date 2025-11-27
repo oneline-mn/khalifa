c8b3763efa8757933f34a829c61b9ef9448d9bbb
a.k.a fl1p <bishu.hover@gmail.com>
2025-11-27 19:06:46 +0800

    shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Khalifa</div>;
+  return (
+    <div>
+      <Button variant={'default'}>Shadcn button</Button>
+      <Button variant={'outline'}>Outline button</Button>
+    </div>
+  );
 }
