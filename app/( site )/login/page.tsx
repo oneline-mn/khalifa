95104ae3914d5387bad9cd0b5579598602db6faa
a.k.a fl1p <bishu.hover@gmail.com>
2025-12-04 11:15:23 +0800

    import fixed

--- app/( site )/login/page.tsx
+++ app/( site )/login/page.tsx
@@ -3,8 +3,8 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import PasswordInput from "@/components/shared/password-input";
 import SignContainer from "@/components/layout/sign-container";
+import PasswordInput from "@/components/shared/password-input";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
