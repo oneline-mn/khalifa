c2b0148aa919249bc2a9f0dcfae8801d459b6248
a.k.a fl1p <bishu.hover@gmail.com>
2025-12-17 14:33:00 +0800

    wrangler jsonc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,14 +17,14 @@ const eslintConfig = defineConfig([
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
   // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-    '.open-next/**',
-  ]),
+  // globalIgnores([
+  //   // Default ignores of eslint-config-next:
+  //   '.next/**',
+  //   'out/**',
+  //   'build/**',
+  //   'next-env.d.ts',
+  //   '.open-next/**',
+  // ]),
 
   {
     ...js.configs.recommended,
